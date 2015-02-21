a8f0de11225da26f1c377d0a0a04933eb57507d6
Tomasz Knapik <tmkn0808@gmail.com>
2015-02-21 15:39:03 +0000

    Language files

--- Upload/inc/languages/english/admin/staff_page.lang.php
+++ Upload/inc/languages/english/admin/staff_page.lang.php
@@ -33,11 +33,11 @@ $l['wrong_group'] = "Choose group again.";
 
 $l['group_deleted'] = "Group has been deleted.";
 $l['group_not_exist'] = "Group does not exist.";
-$l['do_you_want_to_delete_group'] = "Do you want to delete "{1}"?";
+$l['do_you_want_to_delete_group'] = "Do you want to delete \"{1}\"?";
 $l['group_saved'] = "Group has been updated.";
 
 $l['user_not_exist'] = "User does not exist.";
-$l['do_you_want_to_delete_member'] = "Do you want to member "{1}"?";
+$l['do_you_want_to_delete_member'] = "Do you want to member \"{1}\"?";
 $l['member_deleted'] = "Member has been deleted.";
 $l['member_saved'] = "Member has been updated.";
 $l['add_group_first'] = "First you need to add a group.";
@@ -49,3 +49,4 @@ $l['enable_showteam_redirection'] = "Redirect showteam.php to the staff page.";
 $l['enable_showteam_redirection_description'] = "You can switch off or switch on the redirection from showteam.php here.";
 
 $l['can_see_staff_page'] = "Can see Staff Page?";
+$l['can_edit_their_description'] = "Can edit their own description?";
--- Upload/inc/languages/english/staff_page.lang.php
+++ Upload/inc/languages/english/staff_page.lang.php
@@ -11,3 +11,10 @@ $l['no_groups'] = "No groups found.";
 
 $l['online'] = "Online";
 $l['offline'] = "Offline";
+
+$l['edit_members_description'] = "Edit member's description";
+$l['specified_member_entry_does_not_exist'] = "This member doesn't exist.";
+$l['member_updated'] = "Member has been updated";
+
+$l['user'] = "User";
+$l['description'] = "Description";
